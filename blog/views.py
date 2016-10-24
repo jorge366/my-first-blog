e9a028da4e07770eb6d4ade8ea9ad2f9e4095442
jorge <jlkrazy366@hotmail.com>
2016-10-24 16:49:47 -0500

    Agrega mas vistas creacion y edicion del blog dentro da la pagina.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
+#ver que no haya espacios en blanco antes sino tabuladores
+
 def post_list(request):
-        return render(request, 'blog/post_list.html', {})
+		aaa = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+		return render(request, 'blog/post_list.html', { 'v_html': aaa })
+		
+def post_detail(request, pk):
+		detalle = get_object_or_404(Post, pk=pk)
+		return render(request, 'blog/post_detail.html', { 'v_html': detalle })
