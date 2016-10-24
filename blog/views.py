9df2e5a58ea6ff81c70b64e2e5a067947363c70e
jorge <jlkrazy366@hotmail.com>
2016-10-24 18:42:46 -0500

    se agrego crear y editar.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 #ver que no haya espacios en blanco antes sino tabuladores
 
@@ -12,3 +14,29 @@ def post_list(request):
 def post_detail(request, pk):
 		detalle = get_object_or_404(Post, pk=pk)
 		return render(request, 'blog/post_detail.html', { 'v_html': detalle })
+		
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('blog.views.post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+	
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog.views.post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
